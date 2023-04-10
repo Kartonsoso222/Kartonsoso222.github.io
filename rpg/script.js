9b2ba3bf8323a65be2950ee6046f39fac12454e7
Kartonsoso222 <120308507+Kartonsoso222@users.noreply.github.com>
2023-04-10 11:13:31 +0200

    Add files via upload

---
+++ rpg/script.js
@@ -0,0 +1,184 @@
+const flee = document.querySelector(".flee")
+const UI = document.querySelector(".UI")
+const options = document.querySelector(".options")
+
+let action = 0
+let actions_amount = 2
+
+let run_selected = 1
+let character_selected = 0
+let action_seleted = 0
+
+let turn = 1
+
+document.querySelector(".flee_list").querySelectorAll("li")[0].style.textDecoration = "underline"
+document.querySelectorAll("#character")[0].style.borderBottom = "1px solid white"
+addEventListener("keydown", (e)=>{
+
+    if(e.key == 'Enter')
+    {
+        if(action != actions_amount && run_selected != 0)
+        {
+            action++
+            if (action == 1)
+            {
+                document.querySelectorAll("#character")[0].style.borderBottom = "1px solid white"
+            }
+        }
+        else if (run_selected == 0 && action == 0)
+        {
+            document.write("ran")
+        }
+        playerAction()
+    }
+    else if (e.key == 'Escape')
+    {
+        if(action != 0)
+        {
+            action--
+        }
+        playerAction()
+    }
+    else if (e.key == 'ArrowDown')
+    {
+        if(action==0)
+        {
+            if(run_selected == 1)
+            {
+                run_selected--
+                fight_or_flight()
+            }
+        }
+        else if (action==1)
+        {
+            if(action_seleted != 3)
+            {
+                action_seleted++
+                playerAction()
+            }
+        }
+    }
+    else if (e.key == 'ArrowUp')
+    {
+        if(action==0)
+        {
+            if(run_selected == 0)
+            {
+                run_selected++
+                fight_or_flight()
+            }
+        }
+        else if (action==1)
+        {
+            if(action_seleted != 0)
+            {
+                action_seleted--
+                playerAction()
+            }
+        }
+    }
+})
+
+// combat
+let testPlayer1 =
+{
+    name: "Character1",
+    hp: 100,
+    dmg: 20,
+    agility: 10,
+    move: function attack(){
+        testEnemy1.hp -= testPlayer1.dmg    
+        if(testEnemy1.hp <= 0)
+        {
+            document.querySelector(".enemy1").style.display = "none"
+            testEnemy1.status = "dead"
+        }
+        document.querySelector(".testHP").innerHTML = testEnemy1.hp
+    }
+}
+
+let testEnemy1 = 
+{
+        name: 'Test',
+        hp: 100,
+        dmg: 10,
+        agility: 5,
+        status: "alive",
+        move: function attack(){
+            if(this.status == "alive")
+            {
+                testPlayer1.hp -= testEnemy1.dmg
+                document.querySelector(".testHP2").innerHTML = testPlayer1.hp
+            }
+        }
+}
+
+function playerAction()
+{
+    if(action==0)
+    {
+        options.style.display = "none"
+        flee.style.display = "block"
+    }
+    if(action==1)
+    {
+        flee.style.display = "none"
+        options.style.display = "flex"
+
+        underlines_clear()
+        underlines_select()
+    }
+    if(action==2)
+    {
+        if(action_seleted == 0)
+        {
+            testPlayer1.move()
+            turn++
+            }
+        else if(action_seleted==1)
+        {
+            skills()
+        }
+        else if(action_seleted==2)
+        {
+            guard()
+        }
+        else if(action_seleted==3)
+        {
+            items()
+        }
+        
+       while (turn%2==0)
+       {
+            testEnemy1.move()
+            turn++
+       } 
+    }
+}
+
+
+
+function underlines_clear()
+{
+    for(i=0; i<4; i++)
+    {
+        document.querySelector(".options").querySelectorAll(".option")[i].style.textDecoration = "none"
+    }
+}
+function underlines_select()
+{
+    document.querySelector(".options").querySelectorAll(".option")[action_seleted].style.textDecoration = "underline"
+}
+function fight_or_flight()
+{
+    if(run_selected == 0)
+    {
+        document.querySelector(".flee_list").querySelectorAll("li")[1].style.textDecoration = "underline"
+        document.querySelector(".flee_list").querySelectorAll("li")[0].style.textDecoration = "none"
+    }
+    if(run_selected == 1)
+    {   
+        document.querySelector(".flee_list").querySelectorAll("li")[0].style.textDecoration = "underline"
+        document.querySelector(".flee_list").querySelectorAll("li")[1].style.textDecoration = "none"
+    }
+}
